e466664ca7bec9ef3682b4d9e23e67ee5a97a195
olesiklesha <olesik736@gmail.com>
2024-02-25 16:35:26 +0300

    feat: solve some tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -167,8 +167,18 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const days = new Date(year, month, 0);
+  let weekends = 0;
+
+  for (let i = 0; i < days; i += 1) {
+    const date = new Date(year, month - 1, i + 1);
+    if (date.getDay() === 5 || date.getDay() === 5) {
+      weekends += 1;
+    }
+  }
+
+  return weekends;
 }
 
 /**
